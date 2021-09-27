6c32839b707856eb21938ae7f886fa78d388b3d1
Nghia <dinhvanhuunghia@gmail.com>
2021-09-27 21:59:51 +0700

    update logo

---
+++ JS/slick.js
@@ -0,0 +1,18 @@
+
+$('.blog-carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 3000,
+});
+
+$('.logo-carousel').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 3000,
+});
--- JS/swiper.js
+++ JS/swiper.js
@@ -1,50 +1,28 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination"
-    }
-  });
-  
-  var testimonialThumbs = new Swiper(".testimonial-thumbs", {
-    spaceBetween: 10,
-    slidesPerView: 3,
-    centeredSlides: true,
-    freeMode: true,
-    // autoplay: true,
-    loop: true,
-    speed: 3000
-  });
-  var testimonialContent = new Swiper(".testimonial-comment", {
-    spaceBetween: 10,
-    // autoplay: true,
-    loop: true,
-    speed: 3000,
-    thumbs: {
-      swiper: testimonialThumbs
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
+var testimonialThumbs = new Swiper(".testimonial-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  centeredSlides: true,
+  freeMode: true,
+  // autoplay: true,
+  loop: true,
+  speed: 3000
+});
+var testimonialContent = new Swiper(".testimonial-comment", {
+  spaceBetween: 10,
+  // autoplay: true,
+  loop: true,
+  speed: 3000,
+  thumbs: {
+    swiper: testimonialThumbs
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
